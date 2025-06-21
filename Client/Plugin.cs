5247826b59db8d04b12979252faeba2dee703111
peinwastaken <legokid173@gmail.com>
2025-06-21 20:29:33 +0300

    increment version

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace PeinRecoilRework
 {
-    [BepInPlugin("com.pein.camerarecoilmod", "PeinRecoilRework", "1.1.1")]
+    [BepInPlugin("com.pein.camerarecoilmod", "PeinRecoilRework", "1.2.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static List<WeaponRecoilData> WeaponRecoils { get; set; }
