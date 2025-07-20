57ef06a4aad6e40e30adfdeb0c5a75e51179ed64
peinwastaken <legokid173@gmail.com>
2025-07-20 22:13:16 +0300

    default values

--- Client/Config/Settings/RecoilAngSettings.cs
+++ Client/Config/Settings/RecoilAngSettings.cs
@@ -13,10 +13,10 @@ namespace PeinRecoilRework.Config.Settings
         {
             string formattedCategory = Category.Format(order, category);
 
-            RecoilAngUpMult = Config.Bind(formattedCategory, "Recoil Rotation Up Mult", 0.5f, new ConfigDescription("Multiplier for hand recoil angle up/down.", new AcceptableValueRange<float>(-5f, 5f), new ConfigurationManagerAttributes { Order = 970 }));
-            RecoilAngSideMult = Config.Bind(formattedCategory, "Recoil Rotation Side Mult", 0.5f, new ConfigDescription("Multiplier for sideways hand recoil.", new AcceptableValueRange<float>(-5f, 5f), new ConfigurationManagerAttributes { Order = 960 }));
+            RecoilAngUpMult = Config.Bind(formattedCategory, "Recoil Rotation Up Mult", 0.3f, new ConfigDescription("Multiplier for hand recoil angle up/down.", new AcceptableValueRange<float>(-5f, 5f), new ConfigurationManagerAttributes { Order = 970 }));
+            RecoilAngSideMult = Config.Bind(formattedCategory, "Recoil Rotation Side Mult", 0.3f, new ConfigDescription("Multiplier for sideways hand recoil.", new AcceptableValueRange<float>(-5f, 5f), new ConfigurationManagerAttributes { Order = 960 }));
             RecoilAngReturnSpeed = Config.Bind(formattedCategory, "Recoil Rotation Return Speed", 4f, new ConfigDescription("Hand recoil return speed.", new AcceptableValueRange<float>(-5f, 5f), new ConfigurationManagerAttributes { Order = 930 }));
-            RecoilAngDamping = Config.Bind(formattedCategory, "Recoil Rotation Damping", 0.6f, new ConfigDescription("Hand recoil damping.", new AcceptableValueRange<float>(-5f, 5f), new ConfigurationManagerAttributes { Order = 920 }));
+            RecoilAngDamping = Config.Bind(formattedCategory, "Recoil Rotation Damping", 0.2f, new ConfigDescription("Hand recoil damping.", new AcceptableValueRange<float>(-5f, 5f), new ConfigurationManagerAttributes { Order = 920 }));
         }
     }
 }
